0b67836d24bd5c8ef40a7d7ec04744b6aeaaf3f6
iagoa <iagoantunes.f@gmail.com>
2021-12-15 15:21:49 -0300

    Iago Antunes Ferreira

--- CursoXamarinALURA/CursoXamarinALURA/MainPage.xaml.cs
+++ CursoXamarinALURA/CursoXamarinALURA/MainPage.xaml.cs
@@ -12,6 +12,10 @@ namespace CursoXamarinALURA
     {
         public string Nome { get; set; }
         public decimal Preco { get; set; }
+        public string PrecoFormatado
+        {
+            get { return string.Format("R${0}",Preco); }
+        }
     }
 
     public partial class MainPage : ContentPage
@@ -25,9 +29,16 @@ namespace CursoXamarinALURA
             {
                 new Veiculo{Nome = "Azera v6",Preco = 60000},
                 new Veiculo{Nome = "Fiesta 2.0",Preco = 50000},
-                new Veiculo{Nome = "HB20 S",Preco = 40000}
+                new Veiculo{Nome = "HB20 S",Preco = 40000},
             };
             this.BindingContext = this;
+        }
+
+        private void listViewVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo)e.Item;
+
+            DisplayAlert("Test Drive",string.Format("Voce clicou no modelo {0} que custa {1}",veiculo.Nome,veiculo.PrecoFormatado),"OK");
         }
     }
 }
