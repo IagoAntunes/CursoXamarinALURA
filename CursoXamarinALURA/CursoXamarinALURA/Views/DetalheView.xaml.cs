e6c0bf0d1ba13090d685f831924cbd2d4adf2e77
iagoa <iagoantunes.f@gmail.com>
2021-12-16 08:28:29 -0300

    Iago Antunes Ferreira

--- CursoXamarinALURA/CursoXamarinALURA/Views/DetalheView.xaml.cs
+++ CursoXamarinALURA/CursoXamarinALURA/Views/DetalheView.xaml.cs
@@ -12,6 +12,32 @@ namespace CursoXamarinALURA.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetalheView : ContentPage
     {
+        private const int FREIO_ABS = 800;
+        private const int AR_CONDICIONADO = 1000;
+        private const int MP3_PLAYER = 500;
+
+        public string TextoFreioABS
+        {
+            get
+            {
+                return string.Format("Freio ABS - R$ {0}",FREIO_ABS);
+            }
+        }        
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return string.Format("Ar Condicionado - R$ {0}", AR_CONDICIONADO);
+            }
+        }        
+        public string TextoMP3Player
+        {
+            get
+            {
+                return string.Format("MP3 Player - R$ {0}", MP3_PLAYER);
+            }
+        }
+
         public Veiculo Veiculo { get; set; }
         public DetalheView(Veiculo veiculo)
         {
