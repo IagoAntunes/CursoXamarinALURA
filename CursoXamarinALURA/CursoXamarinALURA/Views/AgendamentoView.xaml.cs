d36ad15850741af4d5db6babbeb3e8363f150d27
iagoa <iagoantunes.f@gmail.com>
2021-12-16 11:05:10 -0300

    Iago Antunes Ferreira

--- CursoXamarinALURA/CursoXamarinALURA/Views/AgendamentoView.xaml.cs
+++ CursoXamarinALURA/CursoXamarinALURA/Views/AgendamentoView.xaml.cs
@@ -16,6 +16,22 @@ namespace CursoXamarinALURA.Views
         public string Nome { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
+
+        DateTime dataAgendamento = DateTime.Today;
+        public DateTime DataAgendamento 
+        {
+            get
+            {
+                return dataAgendamento;
+            }
+            set
+            {
+                dataAgendamento = value;
+            }
+        }
+        public TimeSpan HoraAgendamento { get; set; }
+
+
         public AgendamentoView(Veiculo veiculo)
         {
             InitializeComponent();
