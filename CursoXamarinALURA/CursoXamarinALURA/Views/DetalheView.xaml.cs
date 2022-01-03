83bce8b6e16a07adfbd4f99a53d58c0b166f37c5
iagoa <iagoantunes.f@gmail.com>
2022-01-03 14:00:10 -0300

    Adicionando Binding

--- CursoXamarinALURA/CursoXamarinALURA/Views/DetalheView.xaml.cs
+++ CursoXamarinALURA/CursoXamarinALURA/Views/DetalheView.xaml.cs
@@ -12,7 +12,33 @@ namespace CursoXamarinALURA.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetalheView : ContentPage
     {
-        public Veiculo Veiculo;
+        public Veiculo Veiculo { get; set; }
+        private const int FREIO_ABS = 800;
+        private const int AR_CONDICIONADO = 1000;
+        private const int MP3_PLAYER = 500;
+
+        public string TextoFreioABS
+        {
+            get
+            {
+                return string.Format("Freio ABS - R$ {0}",FREIO_ABS);
+            }
+        }       
+        public string TextoArCondicionado
+        {
+            get
+            {
+                return string.Format("ArCondicionado - R$ {0}",AR_CONDICIONADO);
+            }
+        }      
+        public string TextoMP3Player
+        {
+            get
+            {
+                return string.Format("MP3 Plater - R$ {0}",MP3_PLAYER);
+            }
+        }
+
         public DetalheView(Veiculo veiculo)
         {
             InitializeComponent();
